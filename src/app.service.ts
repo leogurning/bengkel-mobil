4d67b3d5fcb6dfdc38e843b03a783240838f37e2
Leonard Mateus <leogurning@gmail.com>
2026-07-23 20:24:17 +0700

    fix: add api key env

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const apikey = process.env.API_KEY || 'API_KEY not found';
+    return `Hello World! ${apikey}`;
   }
 }
